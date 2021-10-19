9678fb173c630d8eb2a966343504e836461ba8ad
JulianSchumacher99 <julian.schumacher@sterntaler.com>
2021-10-19 12:34:53 +0200

    ✨Added CSS and html structure

--- main.js
+++ main.js
@@ -1,6 +1,65 @@
-import './style.css'
+import { createElement } from "./lib/elements";
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function renderApp() {
+  const appElement = document.querySelector("#app");
+
+  const navElement = createElement(
+    "nav",
+    {
+      className: "nav",
+    },
+    [
+      createElement("a", {
+        textContent: "Docs",
+        className: "nav_docs",
+      }),
+
+      createElement("a", {
+        textContent: "About",
+        className: "nav_about",
+      }),
+
+      createElement("a", {
+        textContent: "HELP US",
+        className: "nav_help",
+      }),
+    ]
+  );
+
+  const headerElement = createElement(
+    "header",
+    {
+      className: "header",
+    },
+    [
+      createElement("h1", {
+        textContent: "The Rick and Morty API",
+      }),
+    ]
+  );
+
+  const mainElement = createElement(
+    "main",
+    {
+      className: "main",
+    },
+    [createElement("p", { textContent: "In development..." })]
+  );
+
+  const footerElement = createElement(
+    "footer",
+    {
+      className: "footer",
+    },
+    [
+      createElement("p", {
+        textContent: "❮❯ by Julian Schumacher 2021",
+      }),
+    ]
+  );
+
+  appElement.append(navElement, headerElement, mainElement, footerElement);
+}
+
+renderApp();
